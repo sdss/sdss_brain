5adddc3b6597343b935e74ceb80df5da92a47de6
Brian Cherinka <havok2063@hotmail.com>
2020-10-19 12:32:43 -0400

    adding example of using db handler in cube tool

--- python/sdss_brain/tools/cubes.py
+++ python/sdss_brain/tools/cubes.py
@@ -13,6 +13,8 @@
 
 from __future__ import print_function, division, absolute_import
 
+import warnings
+from astropy.io import fits
 from sdss_brain.tools import Spectrum
 from sdss_brain.helpers import sdss_loader
 
@@ -27,3 +29,37 @@ class Cube(Spectrum):
 class MangaCube(Spectrum):
     """ Class representing a MaNGA IFU datacube for a single galaxy """
     specutils_format: str = 'MaNGA cube'
+
+    def _load_object_from_db(self, data=None):
+        if not self._db.connected:
+            return
+
+        # make a database call to retrieve the Cube row
+        self.data = self._db.session.query(self._db.model).join(self._db.models.IFUDesign,
+                                                                self._db.models.PipelineInfo,
+                                                                self._db.models.PipelineVersion).\
+            filter(self._db.model.plateifu == self.plateifu,
+                   self._db.models.PipelineVersion.version == self.drpver).one_or_none()
+
+        if not self.data:
+            warnings.warn(f"No data returned from database for {self.plateifu}")
+            return
+
+        # extract header info and reformat to proper fits.Header
+        # TODO move this into the datamodel product
+        head = [(k, int(v) if v.isnumeric() else float(v) if v.replace('.', '').isnumeric(
+        ) else v.strip(), self.data.header.comments[i]) for i, (k, v) in enumerate(self.data.header.items())]
+        self.header = fits.Header(head)
+
+        # TODO - slow; 5 seconds to access all 3 from local db; improve this?
+        flux = self.data.get3DCube('flux')
+        ivar = self.data.get3DCube('ivar')
+        mask = self.data.get3DCube('mask')
+
+        # construct a new astropy FITS HDUList
+        hdulist = fits.HDUList([fits.PrimaryHDU(data=None), fits.ImageHDU(flux, header=self.header),
+                                fits.ImageHDU(ivar, name='IVAR'), fits.ImageHDU(mask, name='MASK'),
+                                fits.ImageHDU(self.data.wavelength.wavelength, name='WAVE')])
+
+        # load the spectrum
+        self._load_spectrum(hdulist)
