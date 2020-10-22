b6bf7ac78d475299ef30596f1dcd4efb7027cd45
Brian Cherinka <havok2063@hotmail.com>
2020-10-22 14:28:20 -0400

    adding initial api auth code

---
+++ python/sdss_brain/auth/__init__.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Filename: __init__.py
+# Project: auth
+# Author: Brian Cherinka
+# Created: Wednesday, 21st October 2020 2:51:55 pm
+# License: BSD 3-clause "New" or "Revised" License
+# Copyright (c) 2020 Brian Cherinka
+# Last Modified: Wednesday, 21st October 2020 2:51:55 pm
+# Modified By: Brian Cherinka
+
+
+from __future__ import print_function, division, absolute_import
+
+from .netrc import *
+from .htpass import *
+from .user import *
